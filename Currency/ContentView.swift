ce11eac46c09bf952d0a261d5e4b1e50f9e483fc
Thành Đỗ Long <thanh.dolong@gmail.com>
2019-10-22 10:19:05 +0200

    Initial commit

---
+++ Currency/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Currency
+//
+//  Created by Thành Đỗ Long on 21/10/2019.
+//  Copyright © 2019 Thành Đỗ Long. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
